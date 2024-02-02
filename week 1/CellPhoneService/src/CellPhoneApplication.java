53ec5d5645547bae5818481d2df193132c979899
Jonathan Caro <jcaro4001@gmail.com>
2024-02-02 14:19:32 -0700

    Various work added and adjusted code

--- week 1/CellPhoneService/src/CellPhoneApplication.java
+++ week 1/CellPhoneService/src/CellPhoneApplication.java
@@ -1,5 +1,11 @@
 public class CellPhoneApplication {
     public static void main(String[] args){
-
+        int serialNumber;
+        String model;
+        String carrier;
+        int phoneNumber;
+        String owner;
     }
+
+
 }
